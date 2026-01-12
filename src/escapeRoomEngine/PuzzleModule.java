82c0f63c08b437293452cfaace46b9d1043160be
MrIntrovertOG <braydoncastle81@gmail.com>
2026-01-12 09:19:12 -0500

    Create PuzzleModule.java

---
+++ src/escapeRoomEngine/PuzzleModule.java
@@ -0,0 +1,23 @@
+package escapeRoomEngine;
+
+public class PuzzleModule {
+
+	public static String desc = "";
+	public static String name = "";
+	public static String solution = "";
+	public static int attempts = 0;
+	public static boolean solved = false;
+	
+	public void createPuzzle(String puzzleType, int id) {
+		
+	}
+	
+	public void solve(String attempt) {
+		
+	}
+	
+	public String getName() { return name; }
+	public String getDescription() { return desc; }
+	public boolean isSolved() { return solved; }
+	public int getAttempt() { return attempts; }
+}
