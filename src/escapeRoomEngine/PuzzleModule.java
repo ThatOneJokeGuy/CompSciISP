e9315e7facc58f4381d25d9e0e010f6ccf3362ef
MrIntrovertOG <braydoncastle81@gmail.com>
2026-01-16 09:04:54 -0500

    Completed the PuzzleModule
    bingus shmingus

--- src/escapeRoomEngine/PuzzleModule.java
+++ src/escapeRoomEngine/PuzzleModule.java
@@ -1,7 +1,15 @@
 package escapeRoomEngine;
 
-public class PuzzleModule {
+/*
+ * Name (Made By): Braydon Castle
+ * Date: Jan 7th - 16th, 2026
+ * Description: This class holds the information regarding each puzzle type, although it does not contain any form
+ * of logic for the puzzles
+ */
+ 
+public class PuzzleModule { //class identifier
 
+	//object variables
 	private String desc = "";
 	private String name = "";
 	private String puzzleType = "";
@@ -11,6 +19,7 @@ public class PuzzleModule {
 	private int puzzleID = 0;
 	private boolean solved = false;
 	
+	//constructor method
 	public PuzzleModule(String name, String desc, String puzzleType, String solution, int attemptsToSolve, int puzzleID, boolean needsItem, boolean solved) {
 		this.name = name;
 		this.desc = desc;
@@ -20,18 +29,22 @@ public class PuzzleModule {
 		this.puzzleID = puzzleID;	
 	}
 	
+	//method for handling the solving of a puzzle
 	public boolean solve(String attempt) {
-		if (attempt.equals(solution)) {
-			solved = true;
+		if (attempt.equals(solution)) { //checks if attempt equals solution
+			solved = true; //sets solved to true
 		}
-		else {
-			solved = false;
+		else { //checks if attempt doesn't equal solution
+			solved = false; //sets solved to false
 		}
-		return solved;
+		return solved; //returns false
 	}
 	
+	//all of the getter and setter methods for the class
 	public String getName() { return name; }
-	public String getDescription() { return desc; }
+	public String getDesc() { return desc; }
+	public String getPuzzleType() { return puzzleType; }
+	public String getSolution() { return solution; }
 	public boolean isSolved() { return solved; }
 	public int getAttempts() { return attempts; }
 	public void setSolved(boolean solved) {this.solved = solved; }
